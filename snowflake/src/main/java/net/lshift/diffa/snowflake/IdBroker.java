a4d6152f20bf54ef1bc7669a2450345325dea339
Anthony Harris <anthony@lshift.net>
2012-11-27 14:12:14 +0000

    Changes as per PR comments: - license headers - public -> private - removal of unwanted reporting - prod/test constant shuffling

--- snowflake/src/main/java/net/lshift/diffa/snowflake/IdBroker.java
+++ snowflake/src/main/java/net/lshift/diffa/snowflake/IdBroker.java
@@ -1,34 +1,46 @@
+/**
+ * Copyright (C) 2012 LShift Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.lshift.diffa.snowflake;
 
 import static java.lang.Math.pow;
 import static java.lang.Math.round;
 
 public class IdBroker {
-  public static final long timestampMask = 0xffffffffffc00000L;
-  public static final long machineIdMask = 0x00000000003ff000L;
-  public static final long sequenceMask = 0x0000000000000fffL;
-
-  public static final int timestampBits = 41;
   public static final int machineBits = 10;
   public static final int sequenceBits = 12;
 
-  public static final long timestampUpperBound = round(pow(2, timestampBits)) - 1;
-  public static final short machineIdUpperBound = (short) (round(pow(2, machineBits)) - 1);
   public static final short sequenceUpperBound = (short) (round(pow(2, sequenceBits)) - 1);
 
   public static final int timestampLShift = machineBits + sequenceBits;
   public static final int machineLShift = sequenceBits;
   public static final int sequenceLShift = 0;
 
-  int machineId;
-  TimeFunction timeFn = SystemTimeFunction.getInstance();
-  long pauseMs = 0L;
-  short sequenceNum = 0;
-  long lastTimestamp = -1L;
-  Object mutex = new Object();
+  private int machineId;
+  private long pauseMs = 0L;
+  private TimeFunction timeFn = SystemTimeFunction.getInstance();
+  private short sequenceNum = 0;
+  private long lastTimestamp = -1L;
+  private Object mutex = new Object();
 
   public IdBroker(int machineId) {
     this.machineId = machineId;
+  }
+
+  public void setTimeFn(TimeFunction timeFn) {
+    this.timeFn = timeFn;
   }
 
   public void setPauseMs(int pause) {
--- snowflake/src/test/scala/net/lshift/diffa/snowflake/IdBrokerSpec.scala
+++ snowflake/src/test/scala/net/lshift/diffa/snowflake/IdBrokerSpec.scala
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2012 LShift Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.lshift.diffa.snowflake
 
 import org.junit.runner.RunWith
@@ -6,9 +21,21 @@ import org.scalacheck.Prop
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.ScalaCheck
 import org.specs2.runner.JUnitRunner
+import java.lang.Math.{round, pow}
 
 @RunWith(classOf[JUnitRunner])
 class IdBrokerSpec extends SpecificationWithJUnit with ScalaCheck {
+  final val timestampMask = 0xffffffffffc00000L
+  final val machineIdMask = 0x00000000003ff000L
+  final val sequenceMask  = 0x0000000000000fffL
+
+  final val timestampBits = 41
+  final val machineBits = 10
+  final val sequenceBits = 12
+
+  final val timestampUpperBound: Long = round(pow(2, timestampBits)) - 1
+  final val machineIdUpperBound: Short = (round(pow(2, machineBits)) - 1).asInstanceOf[Short]
+  final val sequenceUpperBound: Short = (round(pow(2, sequenceBits)) - 1).asInstanceOf[Short]
 
   def retryEvery(retryInterval: Long)(fn: => Unit) {
     try {
@@ -47,39 +74,38 @@ class IdBrokerSpec extends SpecificationWithJUnit with ScalaCheck {
       (1 to targetExecutionCount) foreach { i =>
         retryEvery(100L) { broker.getId() }
       }
-      println("Generated %d in %d milliseconds".format(targetExecutionCount, System.currentTimeMillis() - startTime))
       (System.currentTimeMillis() - startTime) must be lessThan targetExecutionTime
     }
 
     "generate at time T an ID that has a timestamp component of T" in {
-      val ts = Gen.choose(1, IdBroker.timestampUpperBound)
+      val ts = Gen.choose(1, timestampUpperBound)
 
       check(Prop.forAll(ts) { timestamp =>
         val broker = new IdBroker(1)
-        broker.timeFn = new TimeFunction { def now = timestamp }
-        ((broker.getId() & IdBroker.timestampMask) >> (IdBroker.machineBits + IdBroker.sequenceBits)) must_== timestamp
+        broker.setTimeFn(new TimeFunction { def now = timestamp })
+        ((broker.getId() & timestampMask) >> (IdBroker.machineBits + IdBroker.sequenceBits)) must_== timestamp
       })
     }
 
     "generate an ID that has a machine ID component matching the generating ID Broker" in {
-      val machineId = Gen.choose(0, IdBroker.machineIdUpperBound)
+      val machineId = Gen.choose(0, machineIdUpperBound)
 
       check(Prop.forAll(machineId) { id =>
         val broker = new IdBroker(id)
-        ((broker.getId() & IdBroker.machineIdMask) >> IdBroker.sequenceBits) must_== id
+        ((broker.getId() & machineIdMask) >> IdBroker.sequenceBits) must_== id
       })
     }
 
     "generate the first ID with a sequence component of 0" in {
-      ((new IdBroker(1)).getId & IdBroker.sequenceMask) should_== 0
+      ((new IdBroker(1)).getId & sequenceMask) should_== 0
     }
 
     "generate a second ID for the same timestamp with a sequence component of 1" in {
       val broker = new IdBroker(1)
-      broker.timeFn = new TimeFunction { def now = 1L }
+      broker.setTimeFn(new TimeFunction { def now = 1L })
 
       broker.getId()
-      (broker.getId() & IdBroker.sequenceMask) should_== 1
+      (broker.getId() & sequenceMask) should_== 1
     }
 
     "generate the (k+1)th ID for the same timestamp with a sequence component of k" in {
@@ -87,10 +113,10 @@ class IdBrokerSpec extends SpecificationWithJUnit with ScalaCheck {
 
       check(Prop.forAll(seqNum) { k =>
         val broker = new IdBroker(1)
-        broker.timeFn = new TimeFunction { def now = 1L }
+        broker.setTimeFn(new TimeFunction { def now = 1L })
 
         (1 to k) foreach { i => broker.getId() }
-        (broker.getId() & IdBroker.sequenceMask) must_== k
+        (broker.getId() & sequenceMask) must_== k
       })
     }
 
@@ -130,9 +156,9 @@ class IdBrokerSpec extends SpecificationWithJUnit with ScalaCheck {
 
     "generate only unique identifiers, even when time runs backwards" in {
       val broker = new IdBroker(1)
-      broker.timeFn = new TimeFunction { def now = 3L }
+      broker.setTimeFn(new TimeFunction { def now = 3L })
       val id = broker.getId()
-      broker.timeFn = new TimeFunction { def now = 2L }
+      broker.setTimeFn(new TimeFunction { def now = 2L })
       try {
         broker.getId()
         failure("getId() should throw an InvalidSystemClockException when its clock runs backwards")
