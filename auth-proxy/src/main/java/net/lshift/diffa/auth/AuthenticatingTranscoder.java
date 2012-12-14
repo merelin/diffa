7e9837b79cedbd6d775764a282f6796710976c1f
Ben Hood <0x6e6562@gmail.com>
2012-12-14 18:01:50 +0000

    Made the query re-writing a bit less dodgy

--- auth-proxy/src/main/java/net/lshift/diffa/auth/AuthenticatingTranscoder.java
+++ auth-proxy/src/main/java/net/lshift/diffa/auth/AuthenticatingTranscoder.java
@@ -1,10 +1,9 @@
 package net.lshift.diffa.auth;
 
 import org.jboss.netty.channel.*;
-import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
-import org.jboss.netty.handler.codec.http.HttpRequest;
-import org.jboss.netty.handler.codec.http.HttpResponse;
-import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+import org.jboss.netty.handler.codec.http.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.FORBIDDEN;
 import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.*;
@@ -12,32 +11,61 @@ import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class AuthenticatingTranscoder extends SimpleChannelUpstreamHandler {
 
+  static Logger log = LoggerFactory.getLogger(AuthenticatingTranscoder.class);
+
   @Override
   public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
 
-    HttpRequest request = (HttpRequest) e.getMessage();
-    String space = extractSpace(request);
+    if (e.getMessage() instanceof HttpRequest) {
 
-    if (isAuthenticated(space)) {
+      HttpRequest request = (HttpRequest) e.getMessage();
+      String space = extractSpace(request);
 
-      rewriteUri(request, System.currentTimeMillis());
+      if (isAuthenticated(space)) {
+
+        rewriteUri(request, System.currentTimeMillis());
+        ctx.sendUpstream(e);
+
+      }
+      else {
+
+        ctx.getPipeline().addLast("encoder", new HttpResponseEncoder());
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, FORBIDDEN);
+        response.setHeader(CONTENT_LENGTH, 0);
+        ChannelFuture future = ctx.getChannel().write(response);
+        future.addListener(ChannelFutureListener.CLOSE);
+
+      }
+
+    }
+    else if (e.getMessage() instanceof HttpChunk) {
+
+      // TODO This is very dodgy - basically we're assuming that the input stream was pre-validated .... fix this
+
       ctx.sendUpstream(e);
 
     }
     else {
-
-      ctx.getPipeline().addLast("encoder", new HttpResponseEncoder());
-      HttpResponse response = new DefaultHttpResponse(HTTP_1_1, FORBIDDEN);
-      response.setHeader(CONTENT_LENGTH, 0);
-      ChannelFuture future = ctx.getChannel().write(response);
-      future.addListener(ChannelFutureListener.CLOSE);
-
+      log.error("Forgot to implement some code: " + e.getMessage());
     }
+
+
 
   }
 
   private void rewriteUri(HttpRequest request, Long endpoint) {
-    String url = String.format("/%s/interview", endpoint);
+    String url;
+    String original = request.getUri();
+    int index = original.indexOf("?");
+
+    if (index > 0) {
+      String query = original.substring(index);
+      url = String.format("/%s/interview%s", endpoint, query);
+    }
+    else {
+      url = String.format("/%s/interview", endpoint);
+    }
+
     request.setUri(url);
   }
 
