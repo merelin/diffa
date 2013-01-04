771415051e18d1bdc70406b814a60d80fa5bd5f7
Anthony Harris <anthony@lshift.net>
2013-01-04 14:48:54 +0000

    281: partition by entity_id (string prefix).

--- kernel/src/main/scala/net/lshift/diffa/kernel/differencing/JooqDomainDifferenceStore.scala
+++ kernel/src/main/scala/net/lshift/diffa/kernel/differencing/JooqDomainDifferenceStore.scala
@@ -26,11 +26,11 @@ import net.lshift.diffa.schema.jooq.DatabaseFacade
 import net.lshift.diffa.schema.jooq.DatabaseFacade.{timestampToDateTime, dateTimeToTimestamp}
 import net.lshift.diffa.schema.Tables._
 import net.lshift.diffa.schema.tables.records.PendingDiffsRecord
+import org.jooq.{RecordMapper, Record, Field, ResultQuery}
+import org.jooq.impl.Factory
 import org.jooq.impl.Factory._
 import net.lshift.diffa.kernel.util.MissingObjectException
-import org.jooq.impl.Factory
 import org.slf4j.LoggerFactory
-import org.jooq._
 import java.lang.{Long => LONG}
 import java.sql.Timestamp
 import net.lshift.diffa.kernel.naming.CacheName
@@ -80,7 +80,7 @@ class JooqDomainDifferenceStore(db: DatabaseFacade,
   val metadata = new PartitionMetadata(DIFFS.getName)
   metadata.withId(DIFFS.SEQ_ID.getName, DIFFS.SEQ_ID.getDataType)
   metadata.withVersion(DIFFS.SEQ_ID.getName, DIFFS.SEQ_ID.getDataType)
-  metadata.partitionBy(DIFFS.DETECTED_AT.getName, DIFFS.DETECTED_AT.getDataType)
+  metadata.partitionBy(DIFFS.ENTITY_ID.getName, DIFFS.ENTITY_ID.getDataType)
 
   val scanDriver = new PartitionAwareDriver(db.dataSource, metadata, db.resolvedDialect)
 
